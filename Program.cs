b1a0ad587bbb9493441184446f8e373286be8da5
Mikael Pajari <mikael.pajari@live.se>
2020-09-14 13:18:49 +0200

    klar

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace kt28H6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] num={1,2,3,4};
+
+         for(int i =0; i<num.Length; i++)
+         {
+            var answer= num[i];
+            int result = answer * 2;
+            System.Console.WriteLine(result);
+         }
+        }
+    }
+}
